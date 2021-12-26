5dfcdfa1cf8a849dc94cdef30fe45634ee31079e
Basili Artomo <artamoshinbasil@yandex.ru>
2021-12-26 20:15:47 +0300

    new branche commit

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 for($i=0;$i<100;$i++){
-    echo $i."<br";
+    echo $i."<br>";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Train</title>
+</head>
+<body>
+    <h1>Hellp my dear frend</h1>
+</body>
+</html>
